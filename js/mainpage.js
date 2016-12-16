91b9e9d734d6bb178becaca7d9fb0bef53b1f081
DushyantShukla <dushyantshukla.93@gmail.com>
2016-12-16 19:28:07 +0530

    UI theme changes and more icons
    Changed UI Theme to dark blue and enabled toggling of tiny mce editor.

--- js/mainpage.js
+++ js/mainpage.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
     //$('.scroll-pane').jScrollPane();
     $('[data-toggle="toggle"]').unbind('click').click(function() {
-      //debugger
+        //debugger
         var selector = $(this).data("target");
         $(selector).toggleClass('in');
     });
@@ -17,57 +17,98 @@ $(document).on('focusin', function(e) {
 });
 
 $(window).on('resize', doresize);
-$(window).on('load',doresize);
+$(window).on('load', doresize);
 
-function doresize(){
-console.log("function called");
-  var winWidth = $(window).width();
-  var contentAreaHeight = $('#mainarea-inner-content').height();
-  contentAreaHeight-=80;
-  console.log(contentAreaHeight);
-if ( winWidth > 900) {
-  console.log("Dushyant 900");
-  tinymce.init({
-      selector: 'textarea',
-      menubar: false,
-      height: contentAreaHeight,
-      plugins: [
-          'advlist autolink lists link charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table contextmenu paste code'
-      ],
-      toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent',
-      content_css: '//www.tinymce.com/csscodepen.min.css'
-  });
+$(function() {
+    $('body').on('click', '#header-new', function() {
+        console.log("show");
+        tinymce.editors['content-textarea'].show();
+    });
+
+    $('body').on('click', '#cancelchangedarticle', function() {
+      console.log("hide");
+      tinymce.editors['content-textarea'].hide();
+    });
+});
+
+function postInitWork(){
+  console.log("color");
+var editor = tinyMCE.getInstanceById('content-textarea');
+editor.getBody().style.backgroundColor = "#FFFF66";
 }
-else if(winWidth <900 && winWidth>396){
-  console.log("Dushyant <900");
-  tinymce.init({
-      selector: 'textarea',
-      menubar: false,
-      height: contentAreaHeight,
-      plugins: [
-          'advlist autolink lists link charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table contextmenu paste code'
-      ],
-      toolbar: 'undo redo | insert | styleselect | bullist numlist outdent indent',
-      content_css: '//www.tinymce.com/csscodepen.min.css'
-  });
-}else if(winWidth <396){
-  console.log("Dushyant <396");
-  tinymce.init({
-      selector: 'textarea',
-      menubar: false,
-      resize: false,
-      height: contentAreaHeight,
-      plugins: [
-          'advlist autolink lists link charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table contextmenu paste code'
-      ],
-      toolbar: 'insert | styleselect | bullist numlist',
-      content_css: '//www.tinymce.com/csscodepen.min.css'
-  });
-}
+
+function doresize() {
+    console.log("function called");
+    var winWidth = $(window).width();
+    var contentAreaHeight = $('#mainarea-inner-content').height();
+    contentAreaHeight -= 44;
+    console.log(contentAreaHeight);
+    if (winWidth > 900) {
+        console.log("Dushyant 900");
+        tinymce.init({
+            selector: '#content-textarea',
+            menubar: false,
+            statusbar: false,
+            resize: false,
+            height: contentAreaHeight,
+            oninit : postInitWork,
+            body_class: 'mceBlackBody',
+            plugins: [
+                'advlist autolink lists link charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table contextmenu paste code'
+            ],
+            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent|fullscreen',
+            content_css: '../css/mainpagecss/tinymcecontentstylesheet.css',
+            setup: function(ed) {
+                ed.on('init', function(e) {
+                    e.target.hide();
+                });
+            }
+        });
+    } else if (winWidth < 900 && winWidth > 396) {
+        console.log("Dushyant <900");
+        tinymce.init({
+            selector: '#content-textarea',
+            menubar: false,
+            statusbar: false,
+            resize: false,
+            height: contentAreaHeight,
+            oninit : postInitWork,
+            plugins: [
+                'advlist autolink lists link charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table contextmenu paste code'
+            ],
+            toolbar: 'undo redo | insert | styleselect | bullist numlist outdent indent',
+            content_css: '//www.tinymce.com/csscodepen.min.css',
+            setup: function(ed) {
+                ed.on('init', function(e) {
+                    e.target.hide();
+                });
+            }
+        });
+    } else if (winWidth < 396) {
+        console.log("Dushyant <396");
+        tinymce.init({
+            selector: '#content-textarea',
+            menubar: false,
+            statusbar: false,
+            resize: false,
+            height: contentAreaHeight,
+            oninit : postInitWork,
+            plugins: [
+                'advlist autolink lists link charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table contextmenu paste code'
+            ],
+            toolbar: 'insert | styleselect | bullist numlist',
+            content_css: '//www.tinymce.com/csscodepen.min.css',
+            setup: function(ed) {
+                ed.on('init', function(e) {
+                    e.target.hide();
+                });
+            }
+        });
+    }
 }
